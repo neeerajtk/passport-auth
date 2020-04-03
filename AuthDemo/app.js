7c3255cdb2b9459c21d73751ec1295bf385fce0f
neeerajtk <neerajtk560@gmail.com>
2020-04-04 00:05:15 +0530

    login functionality added

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -15,7 +15,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 app.set('view engine','ejs');
-
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(require("express-session")({
     secret: "Neeraj lets u in",
     resave: false,
@@ -25,7 +25,7 @@ app.use(require("express-session")({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -37,6 +37,33 @@ app.get("/secret",function(req,res){
     res.render("secret");
 });
 
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+app.post("/register",function(req,res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}),req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req,res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.post("/login",passport.authenticate("local",{
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req,res){
+
+});
 
 
 app.listen(3000,function(){
