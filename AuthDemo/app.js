8ddddcbe6bd169bdb22b1914d106bc760b9739eb
neeerajtk <neerajtk560@gmail.com>
2020-04-03 23:28:38 +0530

    passport initialised

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,8 +1,33 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    User    = require("./models/user"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
 mongoose.connect("mongodb://localhost/auth_demo_app");
+
 var app = express();
 app.set('view engine','ejs');
+
+app.use(require("express-session")({
+    secret: "Neeraj lets u in",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",function(req,res){
     res.render("home");
